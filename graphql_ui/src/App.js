781d1d71e0962e3e21c5e66367300ce7f74e4307
sviatoslv <sviatoslav.boguslavskyi@gmail.com>
2020-05-05 15:40:04 +0300

    Appolo client added

--- graphql_ui/src/App.js
+++ graphql_ui/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BookList } from './components/BooksList';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://192.168.0.103:3500/graphql',
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="App-header">
-        Graphql React List
-      </h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1 className="App-header">Graphql React List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
